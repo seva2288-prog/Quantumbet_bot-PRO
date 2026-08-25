a30bb8780c25bb8dfc6ea6bf2ff672a405cd08be
seva2288-prog <seva2288@gmail.com>
2026-08-26 00:45:06 +0300

    Use environment variable for app port configuration

--- web_app.py
+++ web_app.py
@@ -1684,4 +1684,5 @@ def export_data():
     return "Нет данных для экспорта", 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
