69918a247799b6ddbba3677a179bed558b2dda5d
seva2288-prog <seva2288@gmail.com>
2026-09-01 13:25:42 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -33,7 +33,7 @@ class Config:
     POSITION_MAX_70 = 15
     FORM_REQUIRED_70 = ['excellent', 'good']
     SKIP_MID_TABLE_70 = True
-    LIMIT_BET_TYPE_70 = 3
+    LIMIT_BET_TYPE_70 = 10
     LIMIT_LEAGUE_70 = 2
     
     # ============================================================
